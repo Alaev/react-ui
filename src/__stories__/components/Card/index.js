3f70491ad4b94bd6100fabfb27c5065a99501a38
Michael Alaev <alaev.michael@gmail.com>
2018-03-31 21:06:59 +0300

    added specs addon to storybook

--- src/__stories__/components/Card/index.js
+++ src/__stories__/components/Card/index.js
@@ -4,11 +4,21 @@ import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
 import { withNotes } from '@storybook/addon-notes';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
+import { specs, describe, it } from 'storybook-addon-specifications'
+import { mount, configure } from "enzyme";
+import expect from "expect";
+import Adapter from 'enzyme-adapter-react-16';
+
+
+configure({ adapter: new Adapter() });
 
 import note from './note.md';
 
-export default (storiesOf) => (
-    storiesOf('Card', module).addDecorator(withKnobs)
+
+
+export default (storiesOf) => {
+
+    return storiesOf('Card', module).addDecorator(withKnobs)
         .add('with text without children', () => <Card></Card>)
         .add('with text children', () => <Card disabled={boolean('Disabled', false)} >😀 😎 👍 💯</Card>)
         .add('with click action', () => <Card actions="true" logAction={action()}> {text('Button Text', 'Change in knobs panel')}<br /></Card>
@@ -19,5 +29,18 @@ export default (storiesOf) => (
                 source: true,
             })(() => <Card>Click the "?" mark at top-right to view the info.</Card>)
         )
+        .add('Hello World', function () {
+            const story = <button onClick={action('Hello World')}>
+                    Hello World
+              </button>;
 
-)
+            specs(() => describe('Hello World', function () {
+                it('Should have the Hello World label', function () {
+                    let output = mount(story);
+                    expect(output.text()).toContain('Hello World');
+                });
+            }));
+
+            return story;
+        })
+}
--- src/__stories__/index.js
+++ src/__stories__/index.js
@@ -4,7 +4,6 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import storiesOfCard from './components/Card';
-
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
