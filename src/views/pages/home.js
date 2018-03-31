9c7e59dc78298a0c9a5d89130601d5c201d02a68
Michael Alaev <alaev.michael@gmail.com>
2018-04-01 00:55:12 +0300

    work in progress added emotion for styels

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -2,7 +2,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { cartOperations } from "../../state/features/cart";
-
+import styled from 'react-emotion';
 import { dictionary } from "../utilities";
 
 const product = { id: 1, name: 'apple' };
@@ -11,8 +11,15 @@ const quantity = 2
 const newQ = 5
 const localStorage = null
 
+const Toast = styled('div')`
+    color:white;
+    background-color:blue;
+    width:200px;
+`;
+
+
 class Home extends Component {
-    componentDidMount() {
+    componentDidMount () {
         if(localStorage) {
             return this.props.setCart(localStorage);
         }
@@ -20,19 +27,18 @@ class Home extends Component {
 
     render () {
         return (
-
             <React.Fragment>
                 <p>{dictionary.title}</p>
 
                 <h2>cart </h2>
-
+                <Toast> Some Toast </Toast>
                 <button onClick={() => this.props.addToCart(product, quantity)}>addToCart</button>
                 <button onClick={() => this.props.addToCart(productB, quantity)}>addSecondOption</button>
                 <button onClick={() => this.props.changeQuantity(productB, newQ)}>updateQuantity</button>
                 <button onClick={() => this.props.removeFromCart(productB.id)}>removeFromCart</button>
                 <button onClick={() => this.props.clearCart()}>clearCart</button>
 
-                    <br/>
+                <br />
                 count item: {this.props.getCartItemQuantity}
                 <pre>{
                     JSON.stringify(this.props.cart, null, 2)
@@ -53,7 +59,7 @@ const mapDispatchToProps = {
     removeFromCart: cartOperations.removeFromCart,
     clearCart: cartOperations.clearCart,
     setCart: cartOperations.setCart,
-   
+
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
