74ed79f07c421da2ed723779813b2e992b871f03
Michael Alaev <alaev.michael@gmail.com>
2018-03-31 20:34:44 +0300

    added knobs addon

--- src/__stories__/components/Card/Card.js
+++ src/__stories__/components/Card/Card.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 
-
 export default class Card extends Component {
 
     state = {
--- src/__stories__/components/Card/index.js
+++ src/__stories__/components/Card/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from './Card'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
 
 import { action } from '@storybook/addon-actions';
 
 
 export default (storiesOf) => (
-    storiesOf('Card', module)
-        .add('with text without childern', () => <Card></Card>)
-        .add('with text children', () => <Card >😀 😎 👍 💯</Card>)
-        .add('with click action', () => <Card actions="true" logAction={action()}>Add and Subtract button<br/></Card>
-        ))
+    storiesOf('Card', module).addDecorator(withKnobs)
+        .add('with text without children', () => <Card></Card>)
+        .add('with text children', () => <Card disabled={boolean('Disabled', false)} >😀 😎 👍 💯</Card>)
+        .add('with click action', () => <Card  actions="true" logAction={action()}> {text('Button Text', 'Change in knobs panel')}<br /></Card>
+        )
+)
