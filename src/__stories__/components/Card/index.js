77cb0e04ab013d5bb55ebeac12f7731b30539b0a
Michael Alaev <alaev.michael@gmail.com>
2018-03-31 20:47:48 +0300

    added withInfo storybook addon

--- src/__stories__/components/Card/index.js
+++ src/__stories__/components/Card/index.js
@@ -3,6 +3,7 @@ import Card from './Card'
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
 import { withNotes } from '@storybook/addon-notes';
 import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
 
 import note from './note.md';
 
@@ -10,8 +11,13 @@ export default (storiesOf) => (
     storiesOf('Card', module).addDecorator(withKnobs)
         .add('with text without children', () => <Card></Card>)
         .add('with text children', () => <Card disabled={boolean('Disabled', false)} >😀 😎 👍 💯</Card>)
-        .add('with click action', () => <Card  actions="true" logAction={action()}> {text('Button Text', 'Change in knobs panel')}<br /></Card>
-        )  
-        .add('With Markdown note', withNotes(note)(() => <Card/>))
+        .add('with click action', () => <Card actions="true" logAction={action()}> {text('Button Text', 'Change in knobs panel')}<br /></Card>
+        )
+        .add('With Markdown note', withNotes(note)(() => <Card />))
+        .add('simple info',
+            withInfo({
+                source: true,
+            })(() => <Card>Click the "?" mark at top-right to view the info.</Card>)
+        )
 
 )
