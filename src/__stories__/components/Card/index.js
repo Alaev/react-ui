f4da2f82ecf3715c24704d9bf64f108db29c367d
Michael Alaev <alaev.michael@gmail.com>
2018-03-31 20:39:46 +0300

    added sotry note

--- src/__stories__/components/Card/index.js
+++ src/__stories__/components/Card/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Card from './Card'
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
-
+import { withNotes } from '@storybook/addon-notes';
 import { action } from '@storybook/addon-actions';
 
+import note from './note.md';
 
 export default (storiesOf) => (
     storiesOf('Card', module).addDecorator(withKnobs)
         .add('with text without children', () => <Card></Card>)
         .add('with text children', () => <Card disabled={boolean('Disabled', false)} >😀 😎 👍 💯</Card>)
         .add('with click action', () => <Card  actions="true" logAction={action()}> {text('Button Text', 'Change in knobs panel')}<br /></Card>
-        )
+        )  
+        .add('With Markdown note', withNotes(note)(() => <Card/>))
+
 )
