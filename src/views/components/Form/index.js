828573c5b4d39fccbf6bdb18b56eb568e2a99b82
Michael Alaev <alaev.michael@gmail.com>
2018-04-13 21:40:58 +0300

    added formik, testing forms

---
+++ src/views/components/Form/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import formik from 'formik';
+
+
+
+class Form extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Form;
