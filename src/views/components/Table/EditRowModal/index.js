199ae17d3098ef3c5152e56c8a7f1d04e91e183d
Michael Alaev <alaev.michael@gmail.com>
2018-04-27 22:24:13 +0300

    todo

--- src/views/components/Table/EditRowModal/index.js
+++ src/views/components/Table/EditRowModal/index.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
+// TODO: 
+// get value from this.props.
+// update the the storage with new value
+// close modal on click add
+// close modal on click out side
+// close modal on blur 
+
 class EditRowModal extends Component {
   render() {
     return <div />;
