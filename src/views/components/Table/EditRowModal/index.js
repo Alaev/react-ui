8fa8bc356217b4904659cca388cf0409757cbcaf
Michael Alaev <alaev.michael@gmail.com>
2018-04-28 18:42:03 +0300

    init modal, state, setState with addapter.

--- src/views/components/Table/EditRowModal/index.js
+++ src/views/components/Table/EditRowModal/index.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
-// TODO: 
+// TODO:
 // get value from this.props.
 // update the the storage with new value
 // close modal on click add
 // close modal on click out side
-// close modal on blur 
+// close modal on blur
 
 class EditRowModal extends Component {
+  state = {
+    open: false
+  };
+
+  show = () => this.setState(prevState => ({ open: !prevState.open }), () => console.debug('modal is shown'));
+
+  hide = () => this.setState(prevState => ({ open: !prevState.open }), () => console.debug('modal is hidden'));
+
   render() {
-    return <div />;
+    return (
+      <React.Fragment>
+        <div>hello from modal</div>
+      </React.Fragment>
+    );
   }
 }
 
