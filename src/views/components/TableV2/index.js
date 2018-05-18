28a5de86c7c98e59dfdd1e9e83ef6526b1f628c5
Michael Alaev <alaev.michael@gmail.com>
2018-05-18 22:39:21 +0300

    react-table test

--- src/views/components/TableV2/index.js
+++ src/views/components/TableV2/index.js
@@ -1,4 +1,42 @@
 // testing react table in action
 // https://github.com/react-tools/react-table
+import React, { Component } from 'react';
+import ReactTable from 'react-table';
 
-import reactTable from 'react-table';
+export default class TableV2 extends Component {
+  render() {
+    const data = [
+      {
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        },
+      },
+    ];
+
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Age',
+        accessor: 'age',
+        Cell: props => <span className="number">{props.value}</span>,
+      },
+      {
+        id: 'friendName',
+        Header: 'Friend Name',
+        accessor: d => d.friend.name,
+      },
+      {
+        Header: props => <span>Friend Age</span>,
+        accessor: 'friend.age',
+      },
+    ];
+
+    return <ReactTable data={data} columns={columns} />;
+  }
+}
--- src/views/pages/main.js
+++ src/views/pages/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Table from '../components/Table';
 import TableProvider from '../../context/table';
-
+import TableV2 from '../components/TableV2';
 class Main extends Component {
   render() {
     return (
@@ -9,7 +9,9 @@ class Main extends Component {
         <h1>React Table component</h1>
         <br />
         <br />
-        <Table Provider={TableProvider} />
+        {/* <Table Provider={TableProvider} /> */}
+
+        <TableV2 />
       </div>
     );
   }
