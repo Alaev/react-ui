28aa98f47d0e9a7e0bf9e1974e5409fbad9990c0
Michael Alaev <alaev.michael@gmail.com>
2018-05-20 16:50:48 +0300

    fixed react-table styles

--- src/views/components/TableV2/index.js
+++ src/views/components/TableV2/index.js
@@ -7,25 +7,35 @@ import * as _map from 'lodash/map';
 export default class TableV2 extends Component {
   render() {
     const { Provider } = this.props;
-    const data = [
-      {
-        name: 'Tanner Linsley',
-        age: 26,
-        friend: {
-          name: 'Jason Maurer',
-          age: 23,
-        },
-      },
-    ];
 
     const columns = [
       {
-        Header: 'Name',
-        accessor: 'name',
+        id: 'id',
+        Header: 'id',
+        accessor: d => (
+          <div className="has-text-centered">
+            {d.id}
+          </div>)
       },
       {
-        Header: 'Age',
-        accessor: 'age',
+        id: 'name',
+        Header: 'Course Name',
+        accessor:  d => (
+          <div className="has-text-centered">
+            {d.name}
+          </div>),
+      },
+      {
+        id: 'date',
+        Header: 'date',
+        accessor:  d => (
+          <div className="has-text-centered">
+            {d.date}
+          </div>),
+      },
+      {
+        Header: 'author',
+        accessor: 'author',
         Cell: props => (
           <div className="has-text-centered">
             <span className="tag is-round is-info">{props.value}</span>
@@ -33,13 +43,20 @@ export default class TableV2 extends Component {
         ),
       },
       {
-        id: 'friendName',
-        Header: 'Friend Name',
-        accessor: d => d.friend.name,
+        id: 'publisher',
+        Header: props => <span>publisher</span>,
+        accessor: d => (
+          <div className="has-text-centered">
+            {d.publisher}
+          </div>),
       },
       {
-        Header: props => <span>Friend Age</span>,
-        accessor: 'friend.age',
+        id: 'watched',
+        Header: props => <span>watched</span>,
+        accessor: d => (
+          <div className="has-text-centered">
+            <input type="checkbox" name="checkbox" defaultChecked={d.watched.value} />
+          </div>),
       },
     ];
 
@@ -48,15 +65,15 @@ export default class TableV2 extends Component {
         <TableContext.Consumer>
           {({ tableRows }) =>
             <React.Fragment>
-            <ReactTable
-              tableRows={tableRows}
-              showPaginationBottom={false}
-              data={tableRows}
-              columns={columns}
-              defaultPageSize={5}
-              className="-striped -highlight"
-            />
-            <span>{_map(tableRows,(t => <pre key={t.id}>{t.name}</pre>))}</span>
+              <ReactTable
+                tableRows={tableRows}
+                showPaginationBottom={false}
+                data={_map(tableRows, t => t)}
+                columns={columns}
+                defaultPageSize={5}
+                className="-striped -highlight"
+              />
+              <span>{JSON.stringify()}</span>
 
             </React.Fragment>
           }
