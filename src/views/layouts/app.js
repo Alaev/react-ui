8332d454d427d5a7ccd9e9a1bf5cf8f7b8dc0fb4
Michael Alaev <alaev.michael@gmail.com>
2018-05-20 16:55:56 +0300

    added styles to nav bar

--- src/views/layouts/app.js
+++ src/views/layouts/app.js
@@ -4,24 +4,31 @@ import routes from '../../routes';
 
 const App = () => (
   <div>
-    <header>
-      <Link to="/">MAIN</Link>
-      <Link to="/home">Home</Link>
+    <header style={{
+      width: '100%',
+      height: '40px',
+      background: '#6cf',
+      paddingLeft: '40px',
+      lineHeight: '40px',
+      fontWeight:'bold'
+    }}>
+      <Link style={{color:'white', paddingRight:'15px'}} to="/">Main</Link>
+      <Link style={{color:'white', paddingRight:'15px'}} to="/home">Home</Link>
     </header>
 
     {routes.map(route => <Route key={route.path} {...route} />)}
 
     <footer style={{
-      position:'absolute',
-      bottom:0,
-      width:'100%',
-      height:'40px',
-      background:'#6cf',
-      textAlign:'right',
-      paddingRight:'10px',
-      lineHeight:'40px',
-      color:'white',
-      fontWeight:'bold'
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+      height: '40px',
+      background: '#6cf',
+      textAlign: 'right',
+      paddingRight: '10px',
+      lineHeight: '40px',
+      color: 'white',
+      fontWeight: 'bold'
     }}>react ui playground</footer>
   </div>
 );
