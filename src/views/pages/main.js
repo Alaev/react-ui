a73d4d06214ca88941d1ff35eee88a8f949f4540
Michael Alaev <alaev.michael@gmail.com>
2018-06-08 23:29:09 +0300

    table 3 setup

--- src/views/pages/main.js
+++ src/views/pages/main.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Table from '../components/Table';
 import TableProvider from '../../context/table';
 import TableV2 from '../components/TableV2';
+import TableV3 from '../components/TableV3';
+
 class Main extends Component {
   render() {
     return (
@@ -9,9 +11,13 @@ class Main extends Component {
         <h1>React Table component</h1>
         <br />
         <br />
+
+        {/* Table one example */}
         {/* <Table Provider={TableProvider} /> */}
 
         <TableV2  Provider={TableProvider}/>
+
+        {/* table 3 example */}
       </div>
     );
   }
