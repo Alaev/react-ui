3963e557788d3d590761fa294273d949e33ff66b
Michael Alaev <alaev.michael@gmail.com>
2018-06-09 22:23:52 +0300

    added nice example of rotation with delay

--- src/views/components/TableV3/index.js
+++ src/views/components/TableV3/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Anime from 'react-anime';
 
-const App = (props) => (
-  <React.Fragment >
-    <Anime delay={(e, i) => i * 100}
-      scale={[.1, 1]}
-      opacity={[0, 1]}>
+const App = props => (
+  <React.Fragment>
+    <Anime
+      delay={(el, index) => (index + 1) * 500}
+      opacity={[0.5, 1]}
+      easing={[0.91, -0.54, 0.29, 1.56]}
+      translateX={250}
+      rotate={540}
+      translateX="50vw">
       <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }} />
       <div style={{ backgroundColor: 'green', width: '100px', height: '100px' }} />
       <div style={{ backgroundColor: 'blue', width: '100px', height: '100px' }} />
     </Anime>
-    <div style={{ backgroundColor: 'black', width: '100px', height: '100px' }}></div>
-  </ React.Fragment>
+    <div style={{ backgroundColor: 'black', width: '100px', height: '100px' }} />
+  </React.Fragment>
 );
 
-export default App
+export default App;
