98ba6ab5aa8e435565d7cdcc796923b41f1d9a04
Michael Alaev <alaev.michael@gmail.com>
2018-06-15 21:51:39 +0300

    playing with toast interactions

--- src/views/components/TableV3/index.js
+++ src/views/components/TableV3/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import Anime from 'react-anime';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const notify = () =>
+  toast('Anime Clicked', {
+    position: toast.POSITION.BOTTOM_RIGHT,
+  });
 
 const App = props => (
   <React.Fragment>
@@ -9,12 +16,15 @@ const App = props => (
       easing={[0.91, -0.54, 0.29, 1.56]}
       translateX={250}
       rotate={540}
-      translateX="50vw">
-      <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }} />
+      translateX="50vw"
+    >
+      <div onClick={notify} style={{ backgroundColor: 'red', width: '100px', height: '100px', color:'white' }} >Click Me</div>
       <div style={{ backgroundColor: 'green', width: '100px', height: '100px' }} />
       <div style={{ backgroundColor: 'blue', width: '100px', height: '100px' }} />
     </Anime>
-    <div style={{ backgroundColor: 'black', width: '100px', height: '100px' }} />
+    <div style={{ backgroundColor: 'black', width: '100px', height: '100px', color:'white' }}>no animation</div>
+
+    <ToastContainer />
   </React.Fragment>
 );
 
